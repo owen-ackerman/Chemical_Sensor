f769b736383f6d05b0cd63c239f32369d4ac190f
Owen Ackerman <ackerman01owen@gmail.com>
2020-11-15 18:56:50 -0500

    Draft

--- SerialSave.py
+++ SerialSave.py
@@ -92,6 +92,7 @@ def ReadWrite():
         data = res
         print(data) #prints data to terminal
         z.writerow(data) #write data to csv file
+        text_area.insert(INSERT, data)
 
 def scanning():
     if state:  # If start button was clicked
@@ -115,17 +116,33 @@ def stop():
 
 root = Tk() #creates tk gui
 root.title("COM State") #title 
-root.geometry("500x200") #window size
+root.geometry("210x500") #window size
 
-app = Frame(root) #where the buttons will go
-app.grid()
+start = Button(text="Open COM Port", command=start, fg="green") #buttons widget.
+stop = Button(text="Close COM Port", command=stop, fg="red")
 
-start = Button(app, text="Open COM Port", command=start, fg="green") #buttons widget.
-stop = Button(app, text="Close COM Port", command=stop, fg="red")
+start.grid(column = 0, row = 0) #places buttons with the .grid() function
+stop.grid(column = 1, row = 0, sticky=W)
 
-start.grid() #places buttons with the .grid() function
-stop.grid()
+w = Label(root,  
+         text = "ScrolledText Widget",  
+         font = ("Times New Roman", 12),  
+         background = 'blue', 
+         padx = 40, 
+         foreground = "white")
+w.grid(row = 1, columnspan=2, sticky=W)
 
-root.after(1000, scanning)  # After 1 second, call scanning
-root.mainloop() #waits for user input, exicutes the input, loops. also checks if the window is destroyed, if yes, break
-
+text_area = st.ScrolledText(root, 
+                            width = 10,  
+                            height = 8,  
+                            font = ("Times New Roman", 
+                                    15)) 
+  
+text_area.grid(column = 0, pady = 10, padx = 15, columnspan = 2)
+ 
+  
+# Making the text read only 
+text_area.configure(state ='disabled') 
+  # After 1 second, call scanning
+root.after(1000, scanning)
+root.mainloop()
