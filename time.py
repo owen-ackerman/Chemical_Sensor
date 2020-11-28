06fda0ca133e0eac372a2554029792e4b6d7c6ae
Owen Ackerman <ackerman01owen@gmail.com>
2020-11-28 17:59:32 -0500

    save

--- time.py
+++ time.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 from time import sleep
 
+x = datetime.now()
+print(x.strftime("%b") + "_" + x.strftime("%d") + "_" + x.strftime("%Y"))
 
-for x in range (0,5):
-    now = datetime.now()
-    seconds = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).seconds
-    print(seconds)
-    sleep(1)
